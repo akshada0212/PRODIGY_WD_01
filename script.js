503b27b6a3e1430971e8b5ccb7683cbf8873468e
akshada0212 <154681142+akshada0212@users.noreply.github.com>
2024-01-01 16:55:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changecolor(element){
+    element.style.backgroundColor='SkyBlue';
+}
+function resetcolor(element){
+    element.style.backgroundColor='Azure';
+}
+function changefont(element){
+    element.style.fontFamily='Lucida Calligraphy';
+}
+function resetfont(element){
+    element.style.fontFamily='Cursive';
+}
